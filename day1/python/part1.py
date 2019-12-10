2076fd42af0ebf7254274d2c76d1c5e75ad5a2dc
Tobias Lisbakken <tl@uio.no>
2019-12-10 09:02:00 +0100

    day 1 part 2

--- day1/python/part1.py
+++ day1/python/part1.py
@@ -2,11 +2,26 @@ from math import floor;
 
 weights = None;
 weigth_tot = 0;
+modules = [];
 
-with open("../modules") as modules:
-    weights = modules.readlines();
+with open("../modules") as module_list:
+    weights = module_list.readlines();
+
+# weights = ["1969"];
 
 for weight in weights:
+    modules.append(int(weight.replace("\n","")));
     weigth_tot += floor(int(weight.replace("\n",""))/3) - 2;
 
-print(weigth_tot);
+print("part 1: %s" % weigth_tot);
+
+
+weigth_tot = 0;
+
+for module in modules:
+    req = max(floor(module/3) - 2,0);
+    while req > 0:
+        weigth_tot += req;
+        req = max(floor(req/3) - 2, 0);
+
+print("part 2: %s" % weigth_tot);
