eb438010d42bf1544f1a5657f8b0dd8208cf837a
user <user@localhost.localdomain>
2020-01-14 08:00:14 +0100

    ops

--- day6/ts/part1.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs");
-var planet = /** @class */ (function () {
-    function planet(parent) {
-        this.is_global_com = false;
-        this.children = [];
-    }
-    return planet;
-}());
-fs.read();
