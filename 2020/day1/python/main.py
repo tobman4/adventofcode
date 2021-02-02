777b637a61bd60f59eb52b139ac2b387569881d4
Tobias Lisbakken <tl@uio.no>
2021-02-02 08:33:23 +0100

    day 1 part 2

--- 2020/day1/python/main.py
+++ 2020/day1/python/main.py
@@ -20,14 +20,33 @@ smallData = loadData()
 bigData = smallData.copy()
 bigData.reverse()
 
-for nr1 in bigData:
-    for nr2 in smallData:
-        s = nr1+nr2
-        print("%s+%s=%s" % (nr1,nr2,s))
-        if(s == 2020):
-            print("Found it!!!")
-            print("%s+%s=%s" % (nr1,nr2,s))
-            print("%s*%s=%s" % (nr1,nr2,nr1*nr2))
-            exit()
-        elif(s > 2020):
-            break
+def part1():
+    for nr1 in bigData:
+        for nr2 in smallData:
+            s = nr1+nr2
+            if(s == 2020):
+                print("Part 1:")
+                print("%s+%s=%s" % (nr1,nr2,s))
+                print("%s*%s=%s" % (nr1,nr2,nr1*nr2))
+                return
+            elif(s > 2020):
+                break
+
+def part2():
+    for nr1 in bigData:
+        for nr2 in smallData:
+            if(nr1+nr2 >= 2020):
+                break
+            else:
+                for nr3 in smallData:
+                    s = nr1+nr2+nr3;
+                    if(s == 2020):
+                        print("part 2")
+                        print("%s+%s+%s=%s" % (nr1,nr2,nr3,s))
+                        print("%s*%s*%s=%s" % (nr1,nr2,nr3,(nr1*nr2*nr3)))
+                        return
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
