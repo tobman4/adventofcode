1346ef7bb1f571e875a70b079603fda6f331a66b
Tobias Lisbakken <tl@uio.no>
2021-02-04 13:25:31 +0100

    day 4 part 1

--- 2020/day3/python/main.py
+++ 2020/day3/python/main.py
@@ -1,17 +1,81 @@
-import passport
+import re
 
-heigthReg = r"hgt:\d{1,}(cm|in){1}"
+from passport import passport
 
-def makePass():
-    return
+heigthReg = re.compile(r"hgt:(\d{1,})(cm|in){0,}")
+bYearReg = re.compile(r"byr:(\d{4})")
+iYearReg = re.compile(r"iyr:(\d{4})")
+eYearReg = re.compile(r"eyr:(\d{4})")
+hColorReg = re.compile(r"hcl:(\w{1,}|[#|0-9|a-f]{1,})")
+eColorReg = re.compile(r"ecl:(\w{1,}|[#|0-9|a-f]{1,})")
+CID = re.compile(r"cid:(\d{1,})") # <------ set length?
+PID = re.compile(r"pid:([{\d{1,}}|#])") # <------ set length?
+
+
+
+
+
+def makePass(data):
+    global heigthReg
+    global bYearReg 
+    global iYearReg 
+    global eYearReg 
+    global hColorReg
+    global eColorReg
+    global CID
+    global PID
+    # to not recompile
+
+    out = passport()
+    out.OG = data
+
+    for i in range(8):
+
+        if(i == 0): curr = PID
+        elif(i == 1): curr = CID
+        elif(i == 2): curr = eColorReg
+        elif(i == 3): curr = hColorReg
+        elif(i == 4): curr = eYearReg
+        elif(i == 5): curr = iYearReg
+        elif(i == 6): curr = bYearReg
+        elif(i == 7): curr = heigthReg
+
+        res = curr.search(data)
+
+        if(res == None):
+            continue
+
+        if(i == 0): out.PID = res.group(1)
+        elif(i == 1): out.CID = res.group(1)
+        elif(i == 2): out.eyeColor = res.group(1)
+        elif(i == 3): out.hairColor = res.group(1)
+        elif(i == 4): out.expirationYear = res.group(1)
+        elif(i == 5): out.issueYear = res.group(1)
+        elif(i == 6): out.birthYear = res.group(1)
+        elif(i == 7): out.height = res.group(1)
+
+    return out;
 
 if __name__ == "__main__":
     data = None
+    isValid = 0
 
     with open("data","r") as file:
         data = file.read()
         data = data.split("\n\n")
+
     
 
+    for passStr in data:
+        passStr = makePass(passStr)
 
-    pass
+        if(passStr.isValid() == "GOOD"):
+            isValid += 1
+        else:
+            print(passStr)
+            print()
+            print(passStr.OG)
+            print("########################")
+
+    
+    print("%s valid passports" % isValid)
--- 2020/day3/python/passport.py
+++ 2020/day3/python/passport.py
@@ -9,31 +9,38 @@ class passport:
         self.PID = None
         self.CID = None
 
-        self._testLoop = [
-            self.birthYear,
-            self.issueYear,
-            self.expirationYear,
-            self.height,
-            self.hairColor,
-            self.eyeColor,
-            self.PID,
-            self.CID
-        ]
-
-        self._reqFields = [
-            self.birthYear,
-            self.issueYear,
-            self.expirationYear,
-            self.height,
-            self.hairColor,
-            self.eyeColor,
-            self.PID,
-            #self.CID
-        ]
+        self.OG = None
         return
     
     def isValid(self):
-        for f in self._reqFields:
-            if(f == None):
-                return False
-        return True
+        """
+        good good code
+        """
+
+        if(self.birthYear == None): return "Missing birthYear"
+        if(self.issueYear == None): return "Missing issueYear"
+        if(self.expirationYear == None): return "Missing expirationYear"
+        if(self.height == None): return "Missing height"
+        if(self.hairColor == None): return "Missing hairColor"
+        if(self.eyeColor == None): return "Missing eyeColor"
+        if(self.PID == None): return "Missing PID"
+        #if(self.CID == None): return "Missing CID"
+
+        return "GOOD"
+
+    def _pythonIsBad(data):
+        if(data == None):
+            return "Null"
+        else:
+            return data
+
+    def __str__(self):
+        out = "CID = %s\n" % self.CID
+        out += "PID = %s\n" % self.PID
+        out += "birth year = %s\n" % self.birthYear
+        out += "issue year = %s\n" % self.issueYear
+        out += "expiration year = %s\n" % self.expirationYear
+        out += "height = %s\n" % self.height # <----- fix cm/in
+        out += "hair color = %s\n" % self.hairColor
+        out += "eye color = %s\n" % self.eyeColor
+        return out;
